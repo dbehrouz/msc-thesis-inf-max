a037c0003c5143f6dfb9511f7090efe41e90b6e0
Behrouz Derakhshan <behrouz.derakhshan@rovio.com>
2014-11-02 18:31:22 +0200

    added new paper,

---
+++ code/giraph-impl/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hadoop jar target/com.behrouz-1.0-SNAPSHOT.jar org.apache.giraph.GiraphRunner "$@"
--- code/giraph-impl/src/main/java/com/bd/propagation/ic/singlecycle/AllPropagationIC.java
+++ code/giraph-impl/src/main/java/com/bd/propagation/ic/singlecycle/AllPropagationIC.java
@@ -8,17 +8,35 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author Behrouz Derakhshan
  */
 public class AllPropagationIC extends BasicComputation<LongWritable, ComplexVertexValue, FloatWritable, LongWritable> {
     public static final LongWritable INFLUENCED = new LongWritable(-10000);
-    public static final LongWritable CONSTANT = new LongWritable(-2000);
+    public static final String INITIAL_SET = "giraph.single.cycle.initial.set";
+    private static List<Long> initialSet;
+
+    public void init() {
+        initialSet = toLong(Arrays.asList(getConf().get(INITIAL_SET, "").split(",")));
+    }
+
+    private List<Long> toLong(List<String> items) {
+        List<Long> longs = new LinkedList<>();
+        for (String item : items) {
+            longs.add(Long.parseLong(item));
+        }
+        return longs;
+    }
+
 
     @Override
     public void compute(Vertex<LongWritable, ComplexVertexValue, FloatWritable> vertex, Iterable<LongWritable> messages) throws IOException {
         if (getSuperstep() == 0) {
+            init();
             // adding my self to the list of influencedBy
             vertex.getValue().getVertexIds().add(vertex.getId().get());
             vertex.setValue(new ComplexVertexValue(1L));
