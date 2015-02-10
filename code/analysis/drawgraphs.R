2467797762f6db248f9a94b17aa1ea98c07e1978
Behrouz Derakhshan <behrouz.derakhshan@rovio.com>
2015-02-10 21:23:10 +0200

    r graphs for time

--- code/analysis/drawgraphs.R
+++ code/analysis/drawgraphs.R
@@ -3,15 +3,24 @@ degreediscount=read.csv(file='degreediscount', header=FALSE,sep=':')
 edgesampling=read.csv(file='edgesampling', header=FALSE,sep=':')
 pagerank=read.csv(file='pagerank', header=FALSE,sep=':')
 random=read.csv(file='random', header=FALSE,sep=':')
+multicycle=read.csv(file='multicycle', header=FALSE,sep=':')
+singlecycle=read.csv(file='singlecycle', header=FALSE,sep=':')
 
 degree[2]=degree[2]/1000
 degreediscount[2]=degreediscount[2]/1000
 edgesampling[2]=edgesampling[2]/1000
 pagerank[2]=pagerank[2]/1000
 random[2]=random[2]/1000
+multicycle[2]=multicycle[2]/1000
+singlecycle[2]=singlecycle[2]/1000
 
 plot(edgesampling, type="l" ,xlab='Seed Size', ylab='Time (s)', lty=1, ylim=c(0,100))
-lines(degree, type="l" , lty=2)
+lines(degree, type="l" , lty=2,)
 lines(degreediscount, type="l" , lty=3)
 lines(pagerank, type="l" , lty=4)
 lines(random, type="l" , lty=5)
+lines(multicycle, type="l" , lty=1, pch=1)
+lines(singlecycle, type="l" , lty=1, pch=2)
+
+
+legend(5,95,c("edgesampling","degree","degreediscount","pagerank","random"), lty =c(1,2,3,4,5),cex=0.7)
--- code/giraph-impl/scripts/run_singleCycleIC.sh
+++ code/giraph-impl/scripts/run_singleCycleIC.sh
@@ -5,6 +5,6 @@ hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar org.apache.giraph.GiraphRunner 
 hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar org.apache.giraph.GiraphRunner com.bd.propagation.function.ic.singlecycle.MultiAttemptSingleCycle -vif com.bd.formats.JsonLongMultiFloatInputFormat -vip /ic/input/col/hep -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat -op /ic/output/hep-multi -w 1
 
 
-hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar com.bd.mapreduce.TopN /ic/output/hep /ic/output/final 40
+hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar com.bd.mapreduce.TopN /ic/output/hep /ic/output/final 100
 
-hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar com.bd.mapreduce.TopN /ic/output/hep-multi /ic/output/final-multi 40
+hadoop jar target/giraph-hadoop-1.0-SNAPSHOT.jar com.bd.mapreduce.TopN /ic/output/hep-multi /ic/output/final-multi 100
