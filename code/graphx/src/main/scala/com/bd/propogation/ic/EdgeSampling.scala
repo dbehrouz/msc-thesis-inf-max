68ff78b7dcebebf1733c52ea819e41243bf0f61d
Behrouz Derakhshan <behrouz.derakhshan@rovio.com>
2015-02-17 19:01:40 +0200

    add unit tests for edge sampling

--- code/graphx/src/main/scala/com/bd/propogation/ic/EdgeSampling.scala
+++ code/graphx/src/main/scala/com/bd/propogation/ic/EdgeSampling.scala
@@ -2,7 +2,8 @@ package com.bd.propogation.ic
 
 import com.bd.SeedFinder
 import org.apache.spark.SparkContext
-import org.apache.spark.graphx.{Graph, VertexId}
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.graphx.{VertexRDD, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 
 
@@ -30,11 +31,10 @@ object EdgeSampling extends SeedFinder {
       // Group by component id
       // a map of component ids to size of component
       // broad cast this variable
-      val ccSize = sc.broadcast(cc.groupBy(_._2).map(c => (c._1, c._2.size.toLong)).collect().toList.toMap)
+      val ccSize = findSizes(cc, sc)
 
-      val vs2 = cc.mapValues(value => ccSize.value.get(value).get).collect.toMap
-      vs = vs ++ vs2.map { case (k, v) => k -> (v + vs.getOrElse(k, 0L))}
-
+      val vs2 = mapVertices(cc, ccSize, sc)
+      vs = addVertices(vs, vs2)
 
       println("Iteration : " + i)
       sampledGraph.unpersistVertices(blocking = false)
@@ -42,6 +42,18 @@ object EdgeSampling extends SeedFinder {
       cc.unpersist(blocking = false)
     }
     graph.unpersist(blocking = false)
-    sc.parallelize(vs.toSeq.sortBy(- _._2).take(seedSize).map(_._1))
+    sc.parallelize(vs.toSeq.sortBy(-_._2).take(seedSize).map(_._1))
+  }
+
+  def findSizes(cc: VertexRDD[VertexId], sc: SparkContext): Broadcast[Map[VertexId, Long]] = {
+    sc.broadcast(cc.groupBy(_._2).map(c => (c._1, c._2.size.toLong)).collect().toList.toMap)
+  }
+
+  def mapVertices(cc: VertexRDD[VertexId], ccSize: Broadcast[Map[VertexId, Long]], sc: SparkContext): Map[VertexId, Long] = {
+    cc.mapValues(value => ccSize.value.get(value).get).collect.toMap
+  }
+
+  def addVertices(vs: Map[VertexId, Long], vs2: Map[VertexId, Long]): Map[VertexId, Long] = {
+    vs ++ vs2.map { case (k, v) => k -> (v + vs.getOrElse(k, 0L))}
   }
 }
--- code/graphx/src/test/scala/com/bd/propogation/ic/EdgeSamplingTest.scala
+++ code/graphx/src/test/scala/com/bd/propogation/ic/EdgeSamplingTest.scala
@@ -1,11 +1,39 @@
 package com.bd.propogation.ic
 
 import com.bd.SparkTestBase
+import org.apache.spark.graphx.{VertexId, VertexRDD}
+import org.junit.{Before, Test}
 
 /**
  * @author Behrouz Derakhshan
  */
 class EdgeSamplingTest extends SparkTestBase {
 
+  var cc: VertexRDD[VertexId] = _
+
+  @Before override def setUp() {
+    super.setUp()
+    cc = VertexRDD(sc.parallelize(List((1L, 1L), (2L, 1L), (3L, 1L), (4L, 1L), (5L, 5L), (6L, 5L), (7L, 5L))))
+  }
+
+  @Test def findCCSize() {
+
+    val mappedValues = EdgeSampling.findSizes(cc, sc)
+    assert(Map(1L -> 4L, 5L -> 3L) == mappedValues.value)
+  }
+
+  @Test def mapVertices() {
+    val broadcastedMap = sc.broadcast(Map(1L -> 4L, 5L -> 3L))
+    val mappedValues = EdgeSampling.mapVertices(cc, broadcastedMap, sc)
+    assert(Map(1L -> 4L, 2L -> 4L, 3L -> 4L, 4L -> 4L, 5L -> 3L, 6L -> 3L, 7L -> 3L) == mappedValues)
+  }
+
+  @Test def addVertices() {
+    val vertices = Map(1L -> 1L, 2L -> 2L, 3L -> 3L, 4L -> 4L, 5L -> 5L, 6L -> 6L, 7L -> 7L)
+    val newVertices = Map(1L -> 4L, 2L -> 4L, 3L -> 4L, 4L -> 4L, 5L -> 3L, 6L -> 3L, 7L -> 3L)
+    val finalVertices = EdgeSampling.addVertices(vertices, newVertices)
+    assert(Map(1L -> 5L, 2L -> 6L, 3L -> 7L, 4L -> 8L, 5L -> 8L, 6L -> 9L, 7L -> 10L) == finalVertices)
+
+  }
 
 }
